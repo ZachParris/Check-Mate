3e61041a08867197671a686dcd183119c7449a20
ZachParris <zacharyjparris@gmail.com>
2017-04-08 22:50:38 -0500

    Added checkmate controller

--- CheckMate/Controllers/CheckMateController.cs
+++ CheckMate/Controllers/CheckMateController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace CheckMate.Controllers
 {
-    public class CheckMateController
+    public class CheckMateController : Controller
     {
+
     }
 }
